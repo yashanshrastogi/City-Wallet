5a895199ffbf3f1d2ec2358d6cd7daa889c45d37
Yashansh Rastogi <yashanshrastogi@gmail.com>
2026-04-26 03:17:03 +0530

    Reset structure

--- dashboard/src/lib/api.ts
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-const API_URL = import.meta.env.VITE_API_URL ?? "http://localhost:8000";
-
-export const api = axios.create({
-  baseURL: API_URL,
-  timeout: 10_000,
-  headers: { "Content-Type": "application/json" },
-});
