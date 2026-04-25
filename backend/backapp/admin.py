abf9657983e23a98fc42cafefc521f5ac9b413cb
Govind S <govindsys1008@gmail.com>
2026-04-26 04:20:57 +0530

    switched to django ninja

--- backend/backapp/admin.py
+++ backend/backapp/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import ArtemisUser
 # Register your models here.
+
+admin.site.register(ArtemisUser)
--- backend/backapp/api.py
+++ backend/backapp/api.py
@@ -1,3 +1,7 @@
 from ninja import NinjaAPI
 
 api=NinjaAPI()
+
+@api.get("/health")
+def chek(request):
+    return {"status": "OK"}
