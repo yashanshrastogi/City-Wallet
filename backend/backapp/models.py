cd3cde1024c9f64e64a76b4de6d0fd024e295830
Govind S <govindsys1008@gmail.com>
2026-04-26 06:34:30 +0530

    contextEnrichment service nearly done

--- backend/backapp/models.py
+++ backend/backapp/models.py
@@ -1,11 +1,11 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
 class ArtemisUser(models.Model):
     email=models.EmailField(unique=True)
     role=models.CharField(blank=True, null=True)
     google_id=models.CharField(unique=True)
-    timestamp=models.DateTimeField(auto_now_add=True)
+    timestamp=models.DateTimeField(auto_new_add=True)
     def __str__(self):
         return f"{self.email}->{self.timestamp}"
 
--- contextEnrichment/main.py
+++ contextEnrichment/main.py
@@ -1,2 +1,38 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+import os, json, base64
+from dotenv import load_dotenv
+load_dotenv()
+import requests
+from google_cloud import pubsub_v1
+cred=os.getenv("cred")
+os.environ["GOOGLE_CREDENTIALS_PATH"]=cred
+publisher=pubsub_v1.PublisherClient()
+CONTENT_TOPIC=os.getenv("CONTENT_TOPIC")
+
 app=FastAPI()
+@app.get("/health")
+def chek():
+    return {"status": "OK"}
+
+@app.post("/enrich")
+async def enr(request: Request):
+    try:
+        body = await request.json()
+        message= body.get("message", {})
+        data=message.get("data")
+        if not data:
+            return {"status": "no data"}
+        dcd=base64.b64decode(data).decode("utf-8")
+        payload=json.loads(dcd)
+        task_id, intent_token=payload.get("task_id"), payload.get("intent_token")
+        geo_zone=payload.get("geo_zone")
+        timestamp=payload.get("timestamp") if payload.get("timestamp") else None
+        res=requests.get(f"https://geocoding-api.open-meteo.com/v1/search?name={geo_zone}")
+        lat, lon= res[0].get("latitude"), res[0].get("longitude")
+        wea=requests.get(f"https://api.open-meteo.com/v1/forecast?latitude={lat}&longitude={lon}&current_weather=true")
+        temp= wea.get("temperature")
+        #TODO: Special days-> after nap
+
+
+
+
